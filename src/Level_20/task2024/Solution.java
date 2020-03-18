c25f62cc35d02d3c2e8dd091e1bad5128c6c0576
zdRusty <lexxwsc@yandex.ru>
2020-03-18 19:33:12 +0500

    add task2024

---
+++ src/Level_20/task2024/Solution.java
@@ -0,0 +1,18 @@
+package Level_20.task2024;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+    Знакомство с графами.
+*/
+
+public class Solution implements Serializable {
+    int node;
+    List<Solution> edges = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+    }
+}
