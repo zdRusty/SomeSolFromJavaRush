213dc6aafb288d2430c8da9d05a0ed6a683fb0ca
zdRusty <lexxwsc@yandex.ru>
2020-04-02 14:02:43 +0500

    add task2112

---
+++ src/Level_21/task2112/DBConnectionManager.java
@@ -0,0 +1,7 @@
+package Level_21.task2112;
+
+public class DBConnectionManager {
+    public FakeConnection getFakeConnection() {
+        return new FakeConnection();
+    }
+}
---
+++ src/Level_21/task2112/Solution.java
@@ -0,0 +1,17 @@
+package Level_21.task2112;
+
+/*
+AutoCloseable и try-with-resources
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        DBConnectionManager dbConnectionManager = new DBConnectionManager();
+        try (FakeConnection fakeConnection = dbConnectionManager.getFakeConnection()) {
+            System.out.println("Entering body of try block.");
+            fakeConnection.usefulOperation();
+            fakeConnection.unsupportedOperation();
+        } catch (Exception e) {
+        }
+    }
+}
