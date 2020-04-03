670d929f1ed356e8a5c49f4657a99a6ec21cccba
zdRusty <lexxwsc@yandex.ru>
2020-04-03 17:40:06 +0500

    add task2113 part 2

--- src/Level_21/task2113/Hippodrome.java
+++ src/Level_21/task2113/Hippodrome.java
@@ -1,7 +1,15 @@
 package Level_21.task2113;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hippodrome {
+    private List<Horse> horses = new ArrayList<>();
     public static void main(String[] args) {
 
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
     }
 }
