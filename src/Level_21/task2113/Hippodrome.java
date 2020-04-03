78c1f507e98ab6faed8452393d746c9dfc1867e3
zdRusty <lexxwsc@yandex.ru>
2020-04-03 18:22:05 +0500

    add task2113 part 6

--- src/Level_21/task2113/Hippodrome.java
+++ src/Level_21/task2113/Hippodrome.java
@@ -1,16 +1,20 @@
 package Level_21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
-    private List<Horse> horses;
-
-    public Hippodrome(List<Horse> horses) {
-        this.horses = horses;
-    }
+    public static Hippodrome game;
+    private List<Horse> horses = new ArrayList<>();
 
     public static void main(String[] args) {
-
+        game = new Hippodrome();
+        Horse h1 = new Horse("h1",3,0);
+        Horse h2 = new Horse("h2",3,0);
+        Horse h3 = new Horse("h3",3,0);
+        game.horses.add(h1);
+        game.horses.add(h2);
+        game.horses.add(h3);
     }
 
     public List<Horse> getHorses() {
