de4948006f9a62daa3752494e0c2148afa5085ec
zdRusty <lexxwsc@yandex.ru>
2020-04-20 19:58:16 +0500

    add task2312 vol.8

--- src/Level_23/task12/Snake.java
+++ src/Level_23/task12/Snake.java
@@ -1,11 +1,18 @@
 package Level_23.task12;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
     private List<SnakeSection> sections;
     private boolean isAlive;
     private SnakeDirection direction;
+
+    public Snake(int x, int y){
+        sections = new ArrayList<>();
+        sections.add(new SnakeSection(x, y));
+        isAlive = true;
+    }
 
     public List<SnakeSection> getSections() {
         return sections;
@@ -21,5 +28,17 @@ public class Snake {
 
     public void setDirection(SnakeDirection direction) {
         this.direction = direction;
+    }
+
+    public int getX(){
+        return sections.get(0).getX();
+    }
+
+    public int getY(){
+        return sections.get(0).getY();
+    }
+
+    public void move(){
+
     }
 }
