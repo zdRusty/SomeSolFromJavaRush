6dc2af4da9512b039dbf8fe3fdf8f587cbd69b93
zdRusty <lexxwsc@yandex.ru>
2020-04-21 18:59:09 +0500

    add task2312 vol.18

--- src/Level_23/task12/SnakeSection.java
+++ src/Level_23/task12/SnakeSection.java
@@ -1,10 +1,9 @@
 package Level_23.task12;
 
-import java.util.Objects;
-
 public class SnakeSection {
     private int x;
     private int y;
+
 
     public SnakeSection(int x, int y) {
         this.x = x;
@@ -15,29 +14,27 @@ public class SnakeSection {
         return x;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
     public int getY() {
         return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof SnakeSection)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
+
         SnakeSection that = (SnakeSection) o;
-        return x == that.x &&
-                y == that.y;
+
+        if (x != that.x) return false;
+        if (y != that.y) return false;
+
+        return true;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(x, y);
+        int result = x;
+        result = 31 * result + y;
+        return result;
     }
 }
