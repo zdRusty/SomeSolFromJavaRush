c1138f91aa04130f32bb986d3f869209d40333a3
zdRusty <lexxwsc@yandex.ru>
2020-04-22 12:34:23 +0500

    add task2303

--- src/Level_23/task01/Solution.java
+++ src/Level_23/task01/Solution.java
@@ -1,15 +1,11 @@
-package Level_23.task03;
+package Level_23.task01;
 
 /*
-Запрети создание экземпляров класса
+Запрети наследование
 */
 public class Solution {
 
-    public static abstract class Listener {
-
-        private Listener(){
-
-        }
+    public static final class Listener {
         public void onMouseDown(int x, int y) {
             // Do something when the mouse down event occurs
         }
--- src/Level_23/task02/Solution.java
+++ src/Level_23/task02/Solution.java
@@ -1,12 +1,12 @@
 package Level_23.task02;
 
 /*
-Запрети наследование
+Запрети переопределение
 */
 public class Solution {
 
-    public static final class Listener {
-        public void onMouseDown(int x, int y) {
+    public static class Listener {
+        public final void onMouseDown(int x, int y) {
             // Do something when the mouse down event occurs
         }
 
