82452e8060f1ea89f92ba9af3ca930a623f0e6f8
zdRusty <lexxwsc@yandex.ru>
2020-04-22 12:00:05 +0500

    add task2301

---
+++ src/Level_23/task02/Solution.java
@@ -0,0 +1,22 @@
+package Level_23.task02;
+
+/*
+Запрети наследование
+*/
+public class Solution {
+
+    public static final class Listener {
+        public void onMouseDown(int x, int y) {
+            // Do something when the mouse down event occurs
+        }
+
+        public void onMouseUp(int x, int y) {
+            // Do something when the mouse up event occurs
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
