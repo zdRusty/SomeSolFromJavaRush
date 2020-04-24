51499df150df0517da7ee5c42eac4bbb356ebd1f
zdRusty <lexxwsc@yandex.ru>
2020-04-24 15:13:33 +0500

    add task2413 vol.2

--- src/Level_24/task13/Arkanoid.java
+++ src/Level_24/task13/Arkanoid.java
@@ -1,7 +1,26 @@
 package Level_24.task13;
 
 public class Arkanoid {
+    private int width;
+    private int height;
+
     public static void main(String[] args) {
 
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
