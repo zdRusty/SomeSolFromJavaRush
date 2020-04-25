d6ccb821583b2c173cb27b86f93bc9d652b03b2d
zdRusty <lexxwsc@yandex.ru>
2020-04-25 14:50:18 +0500

    add task2413 vol.14

--- src/Level_24/task13/Ball.java
+++ src/Level_24/task13/Ball.java
@@ -20,12 +20,19 @@ public class Ball extends BaseObject{
 
     @Override
     public void draw(Canvas canvas) {
-
+        canvas.setPoint(x,y,'O');
     }
 
     @Override
     public void move() {
+        if(!isFrozen){
+            x+=dx;
+            y+=dy;
+        }
+    }
 
+    public void start(){
+        isFrozen=false;
     }
 
     public double getSpeed() {
