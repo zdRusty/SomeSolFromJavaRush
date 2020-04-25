a52891514667c93af893cd97a38d406a2a8a3ae2
zdRusty <lexxwsc@yandex.ru>
2020-04-25 14:25:13 +0500

    add task2413 vol.13

--- src/Level_24/task13/Ball.java
+++ src/Level_24/task13/Ball.java
@@ -1,8 +1,21 @@
 package Level_24.task13;
 
 public class Ball extends BaseObject{
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
     public Ball(double x, double y, double radius) {
         super(x, y, radius);
+    }
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        isFrozen = true;
     }
 
     @Override
@@ -13,5 +26,37 @@ public class Ball extends BaseObject{
     @Override
     public void move() {
 
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setDy(double dy) {
+        this.dy = dy;
     }
 }
