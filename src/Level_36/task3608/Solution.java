2d1ebfb83da3687111a2447f6d555acc1a8f5bd8
zdRusty <lexxwsc@yandex.ru>
2020-05-04 15:26:44 +0500

    add task3608 vol.4

--- src/Level_36/task3608/Solution.java
+++ src/Level_36/task3608/Solution.java
@@ -1,13 +1,13 @@
 package Level_36.task3608;
 
 import Level_36.task3608.controller.Controller;
-import Level_36.task3608.model.FakeModel;
+import Level_36.task3608.model.MainModel;
 import Level_36.task3608.model.Model;
 import Level_36.task3608.view.UsersView;
 
 public class Solution {
     public static void main(String[] args) {
-        Model model = new FakeModel();
+        Model model = new MainModel();
         UsersView usersView = new UsersView();
         Controller controller = new Controller();
 
