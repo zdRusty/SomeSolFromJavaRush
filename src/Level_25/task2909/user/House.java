54d4c5e8febad05a580afdb1d808d90c8e839c18
zdRusty <lexxwsc@yandex.ru>
2020-05-12 14:10:32 +0500

    add task2909 vol.1

---
+++ src/Level_25/task2909/user/House.java
@@ -0,0 +1,5 @@
+package Level_25.task2909.user;
+
+public class House {
+    public String house;
+}
---
+++ src/Level_25/task2909/user/Work.java
@@ -0,0 +1,13 @@
+package Level_25.task2909.user;
+
+public class Work {
+    private String boss;
+
+    public String getBoss() {
+        return boss;
+    }
+
+    public void setBoss(String boss) {
+        this.boss = boss;
+    }
+}
