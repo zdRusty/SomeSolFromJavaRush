5dfe5742e965f02f4fc6cd3d5e04d8a479d00008
zdRusty <lexxwsc@yandex.ru>
2020-05-13 17:17:05 +0500

    add task2909 vol.11

--- src/Level_25/task2909/car/Car.java
+++ src/Level_25/task2909/car/Car.java
@@ -23,21 +23,14 @@ public class Car {
         this.numberOfPassengers = numberOfPassengers;
     }
 
-    public int fill(double numberOfLiters) {
+    public void fill(double numberOfLiters) throws Exception{
         if (numberOfLiters < 0)
-            return -1;
+            throw new Exception();
         fuel += numberOfLiters;
-        return 0;
     }
 
     public double getTripConsumption(Date date, int length, Date SummerStart, Date SummerEnd) {
-        double consumption;
-        if (date.before(SummerStart) || date.after(SummerEnd)) {
-            consumption = length * winterFuelConsumption + winterWarmingUp;
-        } else {
-            consumption = length * summerFuelConsumption;
-        }
-        return consumption;
+        return (isSummer(date,SummerStart,SummerEnd))? getSummerConsumption(length) : getWinterConsumption(length);
     }
 
     public int getNumberOfPassengersCanBeTransferred() {
@@ -87,6 +80,18 @@ public class Car {
             case (CABRIOLET): return new Cabriolet(numberOfPassengers);
             default: return null;
         }
+    }
+
+    public boolean isSummer(Date date, Date summerStart, Date summerEnd){
+        return (date.getTime()>summerStart.getTime()&&date.getTime()<summerEnd.getTime());
+    }
+
+    public double getWinterConsumption(int length){
+        return length * winterFuelConsumption + winterWarmingUp;
+    }
+
+    public double getSummerConsumption(int length){
+        return length * summerFuelConsumption;
     }
 }
 
