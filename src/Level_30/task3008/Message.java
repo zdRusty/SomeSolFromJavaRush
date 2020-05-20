e1431864bddb3e8941737b6c56dafc52fb95d088
zdRusty <lexxwsc@yandex.ru>
2020-05-20 17:03:00 +0500

    add task3008 vol.1

---
+++ src/Level_30/task3008/Message.java
@@ -0,0 +1,4 @@
+package Level_30.task3008;
+
+public class Message {
+}
---
+++ src/Level_30/task3008/Server.java
@@ -0,0 +1,7 @@
+package Level_30.task3008;
+
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
