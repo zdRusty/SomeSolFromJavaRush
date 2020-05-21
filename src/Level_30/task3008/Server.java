d16e6624733818c017fbd23c89493f571b577959
zdRusty <lexxwsc@yandex.ru>
2020-05-21 14:06:53 +0500

    add task3008 vol.6

--- src/Level_30/task3008/Server.java
+++ src/Level_30/task3008/Server.java
@@ -1,7 +1,27 @@
 package Level_30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args){
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())){
+            ConsoleHelper.writeMessage("Server started");
+            while (true){
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
