45571376bf101cf2a1da172c8633ddffecf0ee5b
zdRusty <lexxwsc@yandex.ru>
2020-05-22 14:47:42 +0500

    add task3008 vol.12

---
+++ src/Level_30/task3008/client/Client.java
@@ -0,0 +1,12 @@
+package Level_30.task3008.client;
+
+import Level_30.task3008.Connection;
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public class SocketThread extends Thread{
+
+    }
+}
