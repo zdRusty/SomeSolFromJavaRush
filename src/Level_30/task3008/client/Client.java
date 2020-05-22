4e4a46b6a338d6e1de17442e98e5e7cbf5dba7e4
zdRusty <lexxwsc@yandex.ru>
2020-05-22 15:47:24 +0500

    add task3008 vol.13

--- src/Level_30/task3008/client/Client.java
+++ src/Level_30/task3008/client/Client.java
@@ -1,10 +1,48 @@
 package Level_30.task3008.client;
 
 import Level_30.task3008.Connection;
+import Level_30.task3008.ConsoleHelper;
+import Level_30.task3008.Message;
+import Level_30.task3008.MessageType;
+
+import java.io.IOException;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Enter server address.");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Enter server port.");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Enter username.");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT,text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Failed to send message");
+            clientConnected=false;
+        }
+    }
+
 
     public class SocketThread extends Thread{
 
