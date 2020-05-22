7651faf3baa180d1c143e938c75e9405909d01c9
zdRusty <lexxwsc@yandex.ru>
2020-05-22 13:48:36 +0500

    add task3008 vol.10

--- src/Level_30/task3008/Server.java
+++ src/Level_30/task3008/Server.java
@@ -65,5 +65,15 @@ public class Server {
                 }
             }
         }
+
+        private void serverMainLoop(Connection connection, String userName)throws IOException,ClassNotFoundException{
+            while (true) {
+                Message message = connection.receive();
+                if(message.getType()==(MessageType.TEXT)){
+                    sendBroadcastMessage(new Message(MessageType.TEXT, userName + ": " + message.getData()));
+                }
+                else ConsoleHelper.writeMessage("ERROR! It's not a text. Try again.");
+            }
+        }
     }
 }
