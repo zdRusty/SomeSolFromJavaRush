b3a505ef690353b8e29558aefe516ff2efea95a0
zdRusty <lexxwsc@yandex.ru>
2020-05-23 13:28:07 +0500

    add task3008 vol.15

--- src/Level_30/task3008/client/Client.java
+++ src/Level_30/task3008/client/Client.java
@@ -73,6 +73,23 @@ public class Client {
     }
 
     public class SocketThread extends Thread{
+        protected void processIncomingMessage(String message){
+            ConsoleHelper.writeMessage(message);
+        }
 
+        protected void informAboutAddingNewUser(String userName){
+            ConsoleHelper.writeMessage(userName+" adding to conversation.");
+        }
+
+        protected void informAboutDeletingNewUser(String userName){
+            ConsoleHelper.writeMessage(userName+" leave out.");
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected){
+            synchronized (Client.this) {
+                Client.this.clientConnected = clientConnected;
+                Client.this.notify();
+            }
+        }
     }
 }
