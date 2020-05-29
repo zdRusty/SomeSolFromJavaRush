4f23687ac2a9ebb5a28f8f91d3f500697c055cdc
zdRusty <lexxwsc@yandex.ru>
2020-05-29 14:54:41 +0500

    add task2707

---
+++ src/Level_27/task2707/Solution.java
@@ -0,0 +1,88 @@
+package Level_27.task2707;
+
+/*
+Определяем порядок захвата монитора
+*/
+public class Solution {
+    public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
+        synchronized (obj1) {
+            synchronized (obj2) {
+                System.out.println(obj1 + " " + obj2);
+            }
+        }
+    }
+
+    public static boolean isLockOrderNormal(final Solution solution, final Object o1, final Object o2) throws Exception {
+        Thread thread1 = new Thread(() -> {
+            synchronized (o2){
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (o1) {
+                    System.out.println("Блокировки нет");
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            solution.someMethodWithSynchronizedBlocks(o2,o1);
+        });
+
+        thread1.setDaemon(true);
+        thread2.setDaemon(true);
+        thread1.start();
+        thread2.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return !thread2.getState().equals(Thread.State.BLOCKED);
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Solution solution = new Solution();
+        final Object o1 = new Object();
+        final Object o2 = new Object();
+
+        System.out.println(isLockOrderNormal(solution, o1, o2));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
