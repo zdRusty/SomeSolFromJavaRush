0de72659a9808f2cbb14a7bde9a8bb98cd0b3f0e
zdRusty <lexxwsc@yandex.ru>
2020-05-30 13:58:43 +0500

    add task2710

---
+++ src/Level_27/task2710/Mail.java
@@ -0,0 +1,15 @@
+package Level_27.task2710;
+
+public class Mail {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
+
+
