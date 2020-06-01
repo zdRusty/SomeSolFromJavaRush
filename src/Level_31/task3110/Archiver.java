c92d4fc8793b606c1f9dc2f45ab7c3981d617573
zdRusty <lexxwsc@yandex.ru>
2020-06-01 17:23:28 +0500

    add task3110 vol.2

--- src/Level_31/task3110/Archiver.java
+++ src/Level_31/task3110/Archiver.java
@@ -1,29 +1,24 @@
 package Level_31.task3110;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Введите путь к архиву");
-        Path archivePath = null;
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            archivePath = Paths.get(br.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ZipFileManager zip = new ZipFileManager(archivePath);
+    public static void main(String[] args) {
 
-        System.out.println("Введите путь к файлу, который будет архивирован");
-        Path filePath = null;
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            filePath = Paths.get(br.readLine());
-        } catch (IOException e) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            System.out.println("Введите путь к архиву");
+            Path archivePath = Paths.get(br.readLine());
+            ZipFileManager zipArchive = new ZipFileManager(archivePath);
+
+            System.out.println("Введите путь к файлу, который будет архивирован");
+            Path filePath = Paths.get(br.readLine());
+            zipArchive.createZip(filePath);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        zip.createZip(filePath);
+
     }
 }
