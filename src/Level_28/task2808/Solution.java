bee937b325a97200be27dbe72642d50d73e621b3
zdRusty <lexxwsc@yandex.ru>
2020-08-03 21:29:29 +0500

    add task2809

--- src/Level_28/task2808/Solution.java
+++ src/Level_28/task2808/Solution.java
@@ -1,29 +1,52 @@
 package Level_28.task2808;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 /*
-ShutdownNow!
+Осваиваем Callable
 */
-
 public class Solution {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        List<Future<String>> futures = new ArrayList<>();
         ExecutorService executor = Executors.newFixedThreadPool(5);
-        for (int i = 1; i <= 10; i++) {
-            final int localId = i;
-            executor.submit(new Runnable() {
-                public void run() {
-                    doExpensiveOperation(localId);
-                }
-            });
+        for (int i = 1000; i <= 1010; i++) {
+            futures.add(executor.submit(getTask(i)));
         }
 
-        executor.shutdownNow().forEach(System.out::println);
+        futures.add(executor.submit(getTask(10000000)));
+
+        for (Future<String> future : futures) {
+            System.out.println(future.get());
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+
+/* output
+500500
+501501
+502503
+503506
+504510
+505515
+506521
+507528
+508536
+509545
+510555
+50000005000000
+*/
     }
 
-    private static void doExpensiveOperation(int localId) {
-        System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
+    public static Callable<String> getTask(final int i) {
+        return new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return String.valueOf(i*(i+1L)/2L);
+            }
+        };
     }
 }
 
