8ec1e205aa766493b0acb27822689e927fd75e9e
zdRusty <lexxwsc@gmail.com>
2021-01-19 13:45:24 +0500

    add task3210

---
+++ src/Level_32/task3210/Solution.java
@@ -0,0 +1,24 @@
+package Level_32.task3210;
+
+/*
+Используем RandomAccessFile
+*/
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.charset.StandardCharsets;
+
+public class Solution {
+    public static void main(String... args) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(args[0],"rw");
+        byte[] buffer = new byte[args[2].toCharArray().length];
+        raf.seek(Long.parseLong(args[1]));
+        raf.read(buffer,0,buffer.length);
+        raf.seek(raf.length());
+        String result = args[2].equals(new String(buffer, StandardCharsets.UTF_8)) ? "true" : "false";
+        raf.write(result.getBytes(StandardCharsets.UTF_8));
+        raf.close();
+    }
+}
+
+
