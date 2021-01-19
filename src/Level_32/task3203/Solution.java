efc29468a136875e80b6407502d579d3590bbbeb
zdRusty <lexxwsc@gmail.com>
2021-01-19 15:08:41 +0500

    add task3203

---
+++ src/Level_32/task3203/Solution.java
@@ -0,0 +1,25 @@
+package Level_32.task3203;
+
+/*
+Пишем стек-трейс
+*/
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = getStackTrace(new IndexOutOfBoundsException("fff"));
+        System.out.println(text);
+    }
+
+    public static String getStackTrace(Throwable throwable) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        throwable.printStackTrace(pw);
+        return sw.toString();
+    }
+}
+
+
+
