d64d319d508406040e1a6e1adb0d2bdb44c92dcb
zdRusty <lexxwsc@gmail.com>
2021-02-03 14:15:52 +0500

    add task3205

---
+++ src/Level_32/task3205/CustomInvocationHandler.java
@@ -0,0 +1,21 @@
+package Level_32.task3205;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class CustomInvocationHandler implements InvocationHandler {
+
+    private SomeInterfaceWithMethods someInterface;
+
+    public CustomInvocationHandler(SomeInterfaceWithMethods someInterface) {
+        this.someInterface = someInterface;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println(method.getName()+" in");
+        Object object = method.invoke(someInterface,args);
+        System.out.println(method.getName()+" out");
+        return object;
+    }
+}
---
+++ src/Level_32/task3205/SomeInterfaceWithMethods.java
@@ -0,0 +1,9 @@
+package Level_32.task3205;
+
+public interface SomeInterfaceWithMethods {
+    void voidMethodWithoutArgs();
+
+    String stringMethodWithoutArgs();
+
+    void voidMethodWithIntArg(int i);
+}
