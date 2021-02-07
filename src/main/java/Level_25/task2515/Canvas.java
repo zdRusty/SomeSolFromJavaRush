56e604afb40a1a42c808ed8fcb2d1b6faaf225c9
zdRusty <lexxwsc@gmail.com>
2021-02-07 19:49:56 +0500

    add task2515 8

--- src/main/java/Level_25/task2515/Canvas.java
+++ src/main/java/Level_25/task2515/Canvas.java
@@ -22,4 +22,40 @@ public class Canvas {
         this.height = height;
         this.matrix = new char[height][width];
     }
+
+    public void setPoint(double x, double y, char c) {
+        int iX = (int) Math.round(x);
+        int iY = (int) Math.round(y);
+
+        if (0 <= iX && iX < matrix[0].length &&
+                0 <= iY && iY < matrix.length) matrix[iY][iX] = c;
+
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if(matrix[j][i]!=0) setPoint(x+i,y+j,c);
+            }
+        }
+    }
+
+    public void clear(){
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                setPoint(i,j,' ');
+            }
+        }
+    }
+
+    public void print(){
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
