9b72bebbb17f76f004ce8680be1bc875a7483859
zdRusty <lexxwsc@gmail.com>
2021-02-07 20:08:06 +0500

    add task2515 11

--- src/main/java/Level_25/task2515/Rocket.java
+++ src/main/java/Level_25/task2515/Rocket.java
@@ -1,7 +1,16 @@
 package Level_25.task2515;
 
 public class Rocket extends BaseObject{
-    public Rocket(double x, double y, double radius) {
-        super(x, y, radius);
+
+    public Rocket(double x, double y) {
+        super(x, y, 1);
+    }
+
+    public void move() {
+        this.setY(this.getY()-1);
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.setPoint(this.getX(),this.getY(),'R');
     }
 }
