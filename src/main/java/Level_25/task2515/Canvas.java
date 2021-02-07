ee08c5a83b6e77ce918b0683994e86817686dd93
zdRusty <lexxwsc@gmail.com>
2021-02-07 19:27:25 +0500

    add task2515 7

--- src/main/java/Level_25/task2515/Canvas.java
+++ src/main/java/Level_25/task2515/Canvas.java
@@ -1,4 +1,25 @@
 package Level_25.task2515;
 
 public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[height][width];
+    }
 }
