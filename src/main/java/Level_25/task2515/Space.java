3758f5b0fec5cc04e70d7bc5e42e9a0c6e9dfa28
zdRusty <lexxwsc@gmail.com>
2021-02-07 18:22:20 +0500

    add task2515 3

--- src/main/java/Level_25/task2515/Space.java
+++ src/main/java/Level_25/task2515/Space.java
@@ -1,6 +1,50 @@
 package Level_25.task2515;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Space {
+
+    private int width;
+    private int height;
+    private SpaceShip ship;
+    private List<Ufo> ufos = new ArrayList<>();
+    private List<Rocket> rockets = new ArrayList<>();
+    private List<Bomb> bombs = new ArrayList<>();
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public SpaceShip getShip() {
+        return ship;
+    }
+
+    public List<Ufo> getUfos() {
+        return ufos;
+    }
+
+    public List<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public List<Bomb> getBombs() {
+        return bombs;
+    }
+
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
+    }
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
     public static void main(String[] args) {
 
     }
