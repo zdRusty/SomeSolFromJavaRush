8ae51eb1626bca89da5dabe326073dd3209a15ec
zdRusty <lexxwsc@gmail.com>
2021-02-09 15:19:56 +0500

    add task3403

---
+++ src/main/java/Level_34/task3403/Solution.java
@@ -0,0 +1,18 @@
+package Level_34.task3403;
+
+/*
+Разложение на множители с помощью рекурсии
+*/
+
+public class Solution {
+    public void recurse(int n) {
+        if (n > 1) {
+            int p = 2;
+            while (n % p != 0) {
+                p++;
+            }
+            System.out.print(p + " ");
+            recurse(n / p);
+        }
+    }
+}
