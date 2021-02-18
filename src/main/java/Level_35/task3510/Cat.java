20d40cb8b58a80a5d82f229ee24e9e818d4d5849
zdRusty <lexxwsc@gmail.com>
2021-02-18 18:31:41 +0500

    add task3510

---
+++ src/main/java/Level_35/task3510/Cat.java
@@ -0,0 +1,15 @@
+package Level_35.task3510;
+
+public class Cat {
+
+    protected String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Кот " + name;
+    }
+}
---
+++ src/main/java/Level_35/task3510/Kitten.java
@@ -0,0 +1,13 @@
+package Level_35.task3510;
+
+public class Kitten extends Cat {
+
+    public Kitten(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Котенок " + name;
+    }
+}
