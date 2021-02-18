824c3467fe1b2786873c5ae0cb82d52c7af918d0
zdRusty <lexxwsc@gmail.com>
2021-02-18 13:14:20 +0500

    add task3502

---
+++ src/main/java/Level_35/task3502/Solution.java
@@ -0,0 +1,16 @@
+package Level_35.task3502;
+
+/*
+Знакомство с дженериками
+*/
+
+import java.util.List;
+
+public class Solution<L extends List<Solution.SomeClass>> {
+    public static class SomeClass <N extends Number> {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
