1e1fc060143486696aada62eba83a4d116f80a9c
zdRusty <lexxwsc@gmail.com>
2021-02-18 13:05:59 +0500

    add task3501

---
+++ src/main/java/Level_35/task3501/GenericStatic.java
@@ -0,0 +1,8 @@
+package Level_35.task3501;
+
+public class GenericStatic {
+    public static <T> T someStaticMethod(T genericObject) {
+        System.out.println(genericObject);
+        return genericObject;
+    }
+}
