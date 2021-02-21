85d3e2cc0e96296cb717ef87cea678550ae6408f
zdRusty <lexxwsc@gmail.com>
2021-02-21 17:36:44 +0500

    add task3702 3

---
+++ src/main/java/level_37/task3702/female/KidGirl.java
@@ -0,0 +1,12 @@
+package level_37.task3702.female;
+
+import level_37.task3702.Human;
+
+public class KidGirl implements Human {
+    public static final int MAX_AGE = 12;
+
+    @Override
+    public String toString() {
+        return "KidGirl{}";
+    }
+}
---
+++ src/main/java/level_37/task3702/female/Woman.java
@@ -0,0 +1,10 @@
+package level_37.task3702.female;
+
+import level_37.task3702.Human;
+
+public class Woman implements Human {
+    @Override
+    public String toString() {
+        return "Woman{}";
+    }
+}
