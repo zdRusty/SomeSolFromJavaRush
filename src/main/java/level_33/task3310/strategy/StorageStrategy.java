3c86fdeaf9d3878447bdcfc6eb37fcc4eb65223e
zdRusty <lexxwsc@gmail.com>
2021-02-25 13:38:03 +0500

    add task3310 2

---
+++ src/main/java/level_33/task3310/strategy/StorageStrategy.java
@@ -0,0 +1,9 @@
+package level_33.task3310.strategy;
+
+public interface StorageStrategy {
+    boolean containsKey(Long key);
+    boolean containsValue(String value);
+    void put(Long key, String value);
+    Long getKey(String value);
+    String getValue(Long key);
+}
