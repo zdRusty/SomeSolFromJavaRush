0877e67d532d680dd314cb0594a9c1ec54ff71da
zdRusty <lexxwsc@gmail.com>
2021-02-25 13:22:26 +0500

    add task3310 1

---
+++ src/main/java/level_33/task3310/Shortener.java
@@ -0,0 +1,12 @@
+package level_33.task3310;
+
+public class Shortener {
+
+    public Long getId(String string){
+        return 0L;
+    }
+
+    public String getString (Long id){
+        return null;
+    }
+}
