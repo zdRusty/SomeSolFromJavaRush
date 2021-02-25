c0b5f4ddc2130b4ef26655afffac62a2153c3e6d
zdRusty <lexxwsc@gmail.com>
2021-02-25 15:09:33 +0500

    add task3310 5

--- src/main/java/level_33/task3310/ExceptionHandler.java
+++ src/main/java/level_33/task3310/ExceptionHandler.java
@@ -2,6 +2,6 @@ package level_33.task3310;
 
 public class ExceptionHandler {
     public static void log(Exception e){
-        System.out.println(e.getMessage());
+        Helper.printMessage(e.getMessage());
     }
 }
